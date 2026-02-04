ef01ed391dde20500b6b3041a5201c465ad41b24
Maurice Lorenz <me@mauricelorenz.com>
2026-02-04 22:35:13 +0100

    Finalize ex4

--- ex4/ft_raise_errors.py
+++ ex4/ft_raise_errors.py
@@ -2,10 +2,29 @@
 
 def check_plant_health(plant_name: str, water_level: int,
                        sunlight_hours: int) -> None:
-    pass
+    """Check if the passed values are valid.
+
+    Args:
+        plant_name: The name of the plant
+        water_level: The current water level
+        sunlight_hours: The time of sunlight exposure in hours
+    """
+    try:
+        if plant_name == "":
+            raise ValueError("Plant name cannot be empty!")
+        elif water_level < 1 or water_level > 10:
+            raise ValueError(f"Water level {water_level} is too high (max 10)")
+        elif sunlight_hours < 2 or sunlight_hours > 12:
+            raise ValueError(f"Sunlight hours {sunlight_hours} "
+                             "is too low (min 2)")
+    except ValueError as e:
+        print(f"Error: {e}\n")
+        return
+    print(f"Plant '{plant_name}' is healthy!\n")
 
 
 def test_plant_checks() -> None:
+    """Call check_plant_health() with valid and invalid values."""
     print("Testing good values...")
     check_plant_health("tomato", 5, 7)
     print("Testing empty plant name...")
